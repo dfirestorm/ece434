df0b3cc67dccbef408004369860db5b3ec875abd
Hau <haudj@rose-hulman.edu>
2021-09-23 16:05:41 -0400

    Create switches_and_buttons.py

---
+++ hw02/switches_and_buttons.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import Adafruit_BBIO.GPIO as GPIO
+import time
+
+buttonP="PAUSE"  # PAUSE or MODE
+buttonM="MODE"
+
+LEDp   ="RED"
+LEDm   ="GREEN"
+
+# Set the GPIO pins:
+GPIO.setup(LEDp,    GPIO.OUT)
+GPIO.setup(LEDm,    GPIO.OUT)
+GPIO.setup(buttonP, GPIO.IN)
+GPIO.setup(buttonM, GPIO.IN)
+
+# Turn on both LEDs
+GPIO.output(LEDp, 1)
+GPIO.output(LEDm, 1)
+# Map buttons to LEDs
+map = {buttonP: LEDp, buttonM: LEDm}
+
+def updateLED(channel):
+    print("channel = " + channel)
+    state = GPIO.input(channel)
+    GPIO.output(map[channel], state)
+    print(map[channel] + " Toggled")
+
+print("Running...")
+
+GPIO.add_event_detect(buttonP, GPIO.BOTH, callback=updateLED) 
+# RISING, FALLING or BOTH
+GPIO.add_event_detect(buttonM, GPIO.BOTH, callback=updateLED)
+
+try:
+    while True:
+        time.sleep(100)   # Let other processes run
+
+except KeyboardInterrupt:
+    print("Cleaning Up")
+    GPIO.cleanup()
+GPIO.cleanup()
