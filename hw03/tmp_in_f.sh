17937a56c84ac09a510a9ee83037a8a3c9517298
Donald Hau <haudj@rose-hulman.edu>
2021-10-01 19:23:46 +0000

    finished shell script

--- hw03/tmp_in_f.sh
+++ hw03/tmp_in_f.sh
@@ -1,3 +1,7 @@
-temp = `i2cget -y 2 0x48 0`
-temp2 = $($temp * 5)
-echo $temp2
+#!/bin/bash
+TEMP=`i2cget -y 2 0x48 0`
+TEMP2=$(($((TEMP * 9/5))+32))
+echo temp 1 is $TEMP2 F
+TEMP=`i2cget -y 2 0x4a 0`
+TEMP2=$(($((TEMP * 9/5))+32))
+echo temp 2 is $TEMP2 F
