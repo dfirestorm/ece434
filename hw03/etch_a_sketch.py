696cc5f928de7b7047549ca3736a31d29335242b
Donald Hau <haudj@rose-hulman.edu>
2021-10-28 20:18:55 +0000

    added encoder controls

--- hw03/etch_a_sketch.py
+++ hw03/etch_a_sketch.py
@@ -7,6 +7,7 @@ Authors Donald Hau.
 """
 
 import Adafruit_BBIO.GPIO as GPIO
+from Adafruit_BBIO.Encoder import RotaryEncoder, eQEP2, eQEP1
 import smbus
 import time
 
@@ -28,6 +29,8 @@ class EtchASketch:
         self.setup()
         self.render()
         self.ended = 0
+        self.pos1 = 0
+        self.pos2 = 0
 
     def setup(self):
         self.bus = smbus.SMBus(2)  # Use i2c bus 1
@@ -37,76 +40,62 @@ class EtchASketch:
         self.bus.write_byte_data(self.matrix, 0x81, 0)   # Disp on, blink off (p11)
         self.bus.write_byte_data(self.matrix, 0xe7, 0)   # Full brightness (page 15)
         self.array_setup()
-        self.button_setup()
+        self.control_setup()
 
     def array_setup(self):
         self.output = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
     
-    def button_setup(self):
-        self.button1="P8_11"
-        self.button2="P8_12"
-        self.button3="P8_15"
-        self.button4="P8_16"
-
-        GPIO.setup(self.button1, GPIO.IN)
-        GPIO.setup(self.button2, GPIO.IN)
-        GPIO.setup(self.button3, GPIO.IN)
-        GPIO.setup(self.button4, GPIO.IN)
+    def control_setup(self):
+        self.encoder1 = RotaryEncoder(eQEP1)
+        self.encoder1.setAbsolute()
+        self.encoder1.enable()
+        self.encoder1.frequency = 1000
         
-    def button_input(self):
-        GPIO.add_event_detect(self.button1, GPIO.BOTH, callback=self.get_input) 
-        # RISING, FALLING or BOTH
-        GPIO.add_event_detect(self.button2, GPIO.BOTH, callback=self.get_input)
-        GPIO.add_event_detect(self.button3, GPIO.BOTH, callback=self.get_input)
-        GPIO.add_event_detect(self.button4, GPIO.BOTH, callback=self.get_input)
+        self.encoder2 = RotaryEncoder(eQEP2)
+        self.encoder2.setAbsolute()
+        self.encoder2.enable()
+        self.encoder2.frequency = 1000
         
     def write_cursor(self):
         self.output[self.x*2] =self.output[self.x*2] | 2**(self.y)
-        print(self.x, self.y)
-        print(self.output)
+        #print(self.x, self.y)
+        #print(self.output)
+        
+    def get_position(self):
+        self.pos1 = self.encoder1.position
+        self.pos2 = self.encoder2.position
     
-    def get_input(self, channel):
-        if channel != None:
-            state = GPIO.input(channel)
-            if state == False:
-                return
-        print("success")
-        if channel == self.button2:
+    def get_input(self):
+        if self.encoder1.position > self.pos1:
             if self.y != 0:
                 self.y -= 1
             self.write_cursor()
-        elif channel == self.button1:
+        elif self.encoder2.position > self.pos2:
             if self.x != 0:
                 self.x -= 1
             self.write_cursor()
-        elif channel == self.button3:
+        elif self.encoder1.position < self.pos1:
             if self.y != self.maxY:
                 self.y += 1
             self.write_cursor()
-        elif channel == self.button4:
+        elif self.encoder2.position < self.pos2:
             if self.x != self.maxX:
                 self.x += 1
             self.write_cursor()
-        elif input_char == 'e':
-            self.array_setup()
-        elif input_char == 'o':
-            response = str(input("Would you like to exit? y/n "))[0]
-            if response == 'y':
-                self.ended = 1
         else:
-            print("press w to move up, a to move left, s to move down, and d to move right.")
-            print("e will shake and o will let you exit or change board size")
-        self.render()
+            return
+        self.get_position()
 
     def render(self):
         self.bus.write_i2c_block_data(self.matrix, 0, self.output)
 
     def run(self):
-        self.button_input()
+        self.render()
         while self.ended == 0:
-            # get_input(None)
-            time.sleep(1/10)
+            self.get_input()
+            self.render()
+            time.sleep(.01)
             
 
 def play():
