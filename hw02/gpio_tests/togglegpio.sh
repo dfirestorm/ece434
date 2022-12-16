5c71f4f041e0b44dee42f59c8950ca37722901dc
Donald Hau <haudj@rose-hulman.edu>
2022-12-16 20:33:35 +0000

    updated etch and gpio toggles

--- hw02/gpio_tests/togglegpio.sh
+++ hw02/gpio_tests/togglegpio.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # From http://wh1t3s.com/2009/05/14/reading-beagleboard-gpio/
 # Orginally /usr/bin/readgpio, Modified by Mark A. Yoder 20-Jul-2011
 #
@@ -11,13 +11,6 @@ fi
 GPIO=$1
 PERIOD=$2
 
-cleanup() { # Set the GPIO port to 0
-  echo 0 > /sys/class/gpio/gpio${GPIO}/value
-  echo "Cleaning up"
-  echo ""
-  exit
-}
-
 # Open the GPIO port
 #
 if [ ! -e /sys/class/gpio/gpio$GPIO ]; then
@@ -25,7 +18,6 @@ if [ ! -e /sys/class/gpio/gpio$GPIO ]; then
 fi
 echo "out" > /sys/class/gpio/gpio${GPIO}/direction
 
-trap cleanup SIGINT # call cleanup on Ctrl-C
 
 THIS_VALUE=0
 NEWLINE=0
@@ -36,24 +28,16 @@ while [ "1" = "1" ]; do
 
   # "^" for high, '_' for low
   if [ "1" = "$THIS_VALUE" ]; then
-    EV="${EV}^"
     THIS_VALUE=0
    else
-    EV="${EV}_"
     THIS_VALUE=1
   fi
   echo $THIS_VALUE > /sys/class/gpio/gpio${GPIO}/value
-#  echo -n $EV
+
 
   # sleep for a while
   sleep $PERIOD
 
-  # wrap line every 72 samples
-  NEWLINE=`expr $NEWLINE + 1`
-  if [ "$NEWLINE" = "72" ]; then
-#    echo ""
-    NEWLINE=0
-  fi
 
 done
 
