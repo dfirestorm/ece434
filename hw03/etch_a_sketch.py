ff77f2cedb01e3727b6f06b3676d22772c903636
Donald Hau <haudj@rose-hulman.edu>
2023-01-10 21:04:47 +0000

    finished hw3

--- hw03/etch_a_sketch.py
+++ hw03/etch_a_sketch.py
@@ -10,8 +10,7 @@ import smbus
 import time
 
 
-def main():
-    play()
+
 
 
 class EtchASketch:
@@ -45,53 +44,80 @@ class EtchASketch:
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
         self.write_cursor()
     
-    def write_cursor(self):
-        self.output[self.x] += 1 << self.y
+    
 
     def control_setup(self):
         left_counter = '/dev/bone/counter/1/count0'
         right_counter = '/dev/bone/counter/2/count0'
-        maxCount = 1000000
+        maxCount = "1000000" # set max counter
+        # with auto-closes files when done
         with open(f'{left_counter}/ceiling', 'w') as f:
             f.write(maxCount)
         with open(f'{right_counter}/ceiling', 'w') as f:
             f.write(maxCount)
+        # enable both counters
         with open(f'{left_counter}/enable', 'w') as f:
-            f.write('1'))
+            f.write('1')
         with open(f'{right_counter}/enable', 'w') as f:
             f.write('1')
+        # open and store encoder positions
         self.left_file = open(f'{left_counter}/count', 'r')
         self.right_file = open(f'{right_counter}/count', 'r')
         self.get_enc_position()
+        # initial position
+        self.output[1+(2*self.x)] = self.output[1+(2*self.x)] ^ (1 << self.y)
 
     def get_enc_position(self):
         self.left_pos = read_data(self.left_file)
         self.right_pos = read_data(self.right_file)
 
     def get_input(self):
-        #write code to determine direction to move here
+        #determine direction to move here
+        left = read_data(self.left_file)
+        right = read_data(self.right_file)
+        # print(f'left {left} from {self.left_pos} right {right} from {self.right_pos} ')
+
+        if left > self.left_pos and self.left_pos > 3: # left encoder rotated right
+            self.use_input(3)
+        elif left < self.left_pos or (self.left_pos < 4 and left > 1000):# left encoder rotated left
+            self.use_input(0)
+        if right > self.right_pos and self.right_pos > 3:# right encoder rotated right
+            self.use_input(2)
+        elif right < self.right_pos or (self.right_pos < 4 and right > 1000):# right encoder rotated left
+            self.use_input(1)
         
 
 
     def use_input(self, button_number): 
-        if button_number == 1:
+        # print(f'moving with button {button_number}')
+        if button_number == 1: # up
             self.move_cursor(-1,0)
             self.write_cursor()
-        elif button_number == 0:
+        elif button_number == 0: # left
             self.move_cursor(0,-1)
             self.write_cursor()
-        elif button_number == 2:
+        elif button_number == 2: # down
             self.move_cursor(1,0)
             self.write_cursor()
-        elif button_number == 3:
+        elif button_number == 3: # right
             self.move_cursor(0,1)
             self.write_cursor()
+        time.sleep(0.05)
+        self.get_enc_position()
     
     def move_cursor(self,y_off,x_off):
+        # disable old pos indicator
+        self.output[1+(2*self.x)] = self.output[1+(2*self.x)] ^ (1 << self.y)
+        # limit to bounds of display
         new_y = constrain(self.y + y_off, 0, self.maxY)
         new_x = constrain(self.x + x_off, 0, self.maxX)
         self.y = new_y
         self.x = new_x
+        # enable new pos indicator
+        self.output[1+(2*self.x)] = self.output[1+(2*self.x)] ^ (1 << self.y)
+
+    def write_cursor(self):
+        self.output[(2*self.x)] = self.output[(2*self.x)] | (1 << self.y)
 
     def render(self):
         self.bus.write_i2c_block_data(self.matrix, 0, self.output)
@@ -109,14 +135,14 @@ def constrain(val, min_val, max_val):
 def read_data(file):
     file.seek(0)
     data = file.read()[:-1]
-    return data
+    return int(data)
 
-def play():
-    s = ""
-    while s != "n":
+def main():
+    try:
         game = EtchASketch()
         game.run()
-        s = str(input("Would you like to exit? y/n"))[0]
+    except KeyboardInterrupt:
+        return 1
 
 
 main()
--- hw03/readme.md
+++ hw03/readme.md
@@ -3,9 +3,13 @@
 ## TMP101
 
  I2C addresses on the I2C Bus: 
- 1001000, 1001001, 1001010
+ 1001000, 1001001, 1001010 
+ 0x48, 0x49, 0x4a
+ Use tmp_in_f.sh to read temp in f
  Use readtemp.py to read two different sensors at once via I2C
+ Use templimit.py to read the sensors with alerts. 
 
 ## Etch A Sketch Updates
 
-The etch a sketch program now uses the 8x8 LED matrix on I2C and uses two rotary encoders to control movement. use setup.sh to ensure using eQEP on the correct pins. 
+The etch a sketch program now uses the 8x8 LED matrix on I2C and uses two rotary encoders to control movement. use setup.sh to ensure using eQEP on the correct pins. No erase, orange as the indicator of user position. 
+Uses p8_33/35 for eQEP1 and p8_41/42 for eQEP2
